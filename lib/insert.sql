ff9d24e074d9414a208e04a00d6c20463b87adc2
Don Odom <divdonis@gmail.com>
2021-09-27 11:54:09 -0700

    commit tree 4426967916ff223bdb65569359c2ae392ab5e272

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, alive) VALUES ("Mr. Chocolate", "M", false);
+INSERT INTO bears (name, sex, alive) VALUES ("Rowdy", "M", true);
+INSERT INTO bears (name, sex, alive) VALUES ("Tabitha", "F", true);
+INSERT INTO bears (name, sex, alive) VALUES ("Sergeant Brown", "M", false);
+INSERT INTO bears (name, sex, alive) VALUES ("Melissa", "F", true);
+INSERT INTO bears (name, sex, alive) VALUES ("Grinch", "M", true);
+INSERT INTO bears (name, sex, alive) VALUES ("Wendy", "F", true);
+INSERT INTO bears (name, sex, alive) VALUES (null, "M", false);
